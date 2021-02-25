06056d91ebefb40cd6f76a198b4b4ee9abb2f34c
ck2136 <chong.kim@ucdenver.edu>
2021-02-25 18:09:32 +0900

    Initial commit

--- dev/01_start.R
+++ dev/01_start.R
@@ -15,12 +15,12 @@
 ## Add meta data about your application
 golem::fill_desc(
   pkg_name = "SurvivalHazardExtraction", # The Name of the package containing the App 
-  pkg_title = "PKG_TITLE", # The Title of the package containing the App 
-  pkg_description = "PKG_DESC.", # The Description of the package containing the App 
-  author_first_name = "AUTHOR_FIRST", # Your First Name
-  author_last_name = "AUTHOR_LAST", # Your Last Name
-  author_email = "AUTHOR@MAIL.COM", # Your Email
-  repo_url = NULL # The URL of the GitHub Repo (optional) 
+  pkg_title = "Extract Hazard Parameters from Survival Analysis for Health Economic Modeling", # The Title of the package containing the App 
+  pkg_description = "", # The Description of the package containing the App 
+  author_first_name = "Chong", # Your First Name
+  author_last_name = "Kim", # Your Last Name
+  author_email = "chong@stratevi.com", # Your Email
+  repo_url = "chk2136/SurvivalHazardExtraction" # The URL of the GitHub Repo (optional) 
 )     
 
 ## Set {golem} options ----
@@ -28,7 +28,7 @@ golem::set_golem_options()
 
 ## Create Common Files ----
 ## See ?usethis for more information
-usethis::use_mit_license( name = "Golem User" )  # You can set another license here
+usethis::use_mit_license(copyright_holder = NULL)  # You can set another license here
 usethis::use_readme_rmd( open = FALSE )
 usethis::use_code_of_conduct()
 usethis::use_lifecycle_badge( "Experimental" )
